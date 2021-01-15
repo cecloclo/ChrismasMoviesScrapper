38e8ca7e4a76b4e5ff176fe7aaffbc5b74b800eb
chloedst <chloe.desmedt@edu.esiee.fr>
2021-01-15 15:11:41 +0100

    init templates

--- newscrawler/chrismasMovies.py
+++ newscrawler/chrismasMovies.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from flask import render_template
+import pymongo
+
+client = pymongo.MongoClient()
+data_films = client['allocine']
+collection = data_films['filmNoel']
+print(collection)
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.errorhandler(404)
+def page_not_found(e):
+    return "ERROR"
+
+@app.route('/') 
+def home_page():
+    return render_template('index.html')
+
+@app.route('/') 
+def search_page():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=2745) 
