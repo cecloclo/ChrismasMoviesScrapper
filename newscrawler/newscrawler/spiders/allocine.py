fe7bbc68a86a7002eaddf02edb6d08ad8b50f819
chloedst <chloe.desmedt@edu.esiee.fr>
2021-02-06 10:16:45 +0100

    changement de la sortie des données

--- newscrawler/newscrawler/spiders/allocine.py
+++ newscrawler/newscrawler/spiders/allocine.py
@@ -9,22 +9,25 @@ class AllocineSpider(scrapy.Spider):
         if string:
             return " ".join(string.split())
 
-
-    def parse(self, response):
+    def scrap(self, response):
 
         for doc in response.xpath('//div[@class="sub-body"]'):
+            
             description_value = doc.css("div .synopsis ::text").extract()
             title_value = doc.css(".entity-card .meta-title ::text").extract()
             eval_spect = doc.css("span.stareval-note ::text").extract()
             image_urls = doc.css('.thumbnail img::attr(data-src)').getall()
-        
 
-            yield {'description' : description_value,
-            'titre' : title_value,
-            'image_urls' : image_urls,
-            'Evaluation spectateur' : eval_spect 
+            yield {
+                'titre' : title_value,
+                'description' : description_value,
+                'image_urls' : image_urls,
+                'Evaluation spectateur' : eval_spect
             }
 
-        for i in range (1,35):
+    def parse(self, response):
+
+        for i in range (0,35):
             next_page_link = 'https://www.allocine.fr/recherche/movie/?q=no%C3%ABl&page='+str(i)
-            yield scrapy.Request(url=next_page_link, callback=self.parse)
+            yield scrapy.Request(url=next_page_link, callback=self.scrap)
+        
