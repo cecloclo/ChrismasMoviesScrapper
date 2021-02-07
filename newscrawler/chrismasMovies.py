e3ba81b403c13486ecf369350b2bffbceb77fb73
Juliette <juliette.rochon@edu.esiee.fr>
2021-02-07 13:49:24 +0100

    Flask remarche

--- newscrawler/chrismasMovies.py
+++ newscrawler/chrismasMovies.py
@@ -1,26 +1,26 @@
 from flask import Flask
 from flask import render_template
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
-import json
+# from elasticsearch import Elasticsearch
+# from elasticsearch.helpers import bulk
+# import json
 
-LOCAL = True
-es_client = Elasticsearch(hosts=["localhost" if LOCAL else "elastic"])
+# LOCAL = True
+# es_client = Elasticsearch(hosts=["localhost" if LOCAL else "elastic"])
 
-with open('dataF.json') as json_data:
-    v = json.load(json_data)
-
-
-def generate_data_exemple(documents):
-    for docu in documents:
-        yield {
-            "_index": "movies",
-            "_type": "movie",
-            "_source": {k: v if v else None for k, v in docu.items()},
-        }
+# with open('dataF.json') as json_data:
+#     v = json.load(json_data)
 
 
-bulk(es_client, generate_data_exemple(v))
+# def generate_data_exemple(documents):
+#     for docu in documents:
+#         yield {
+#             "_index": "movies",
+#             "_type": "movie",
+#             "_source": {k: v if v else None for k, v in docu.items()},
+#         }
+
+
+# bulk(es_client, generate_data_exemple(v))
 
 # def search_by_eval(eval):
 #   QUERY = {
@@ -45,28 +45,29 @@ def home_page():
     return render_template('index.html')
 
 
-@app.route('/search_page/', methods=['GET', 'POST'])
+@app.route('/search_page/')#, methods=['GET', 'POST'])
 def search_page():
     return render_template('elements.html')
 
 
 @app.route('/info_page/')
 def info_page():
-
-    QUERY = {
-        "query": {
-            "match": {
-                #"fields": {'titre'          }
-                "sort": [
-                    {"Evaluation spectateur": {"order": "desc"}}
-                    ]
-            }
-        },
-    }
-    result = es_client.search(index="movies", body=QUERY)
-    {elt['_source']['title']: elt['_source']['popularity'] for elt in result["hits"]["hits"]}
     return render_template('generic.html')
 
+    # QUERY = {
+    #     "query": {
+    #         "match": {
+    #             #"fields": {'titre'          }
+    #             "sort": [
+    #                 {"Evaluation spectateur": {"order": "desc"}}
+    #                 ]
+    #         }
+    #     },
+    # }
+    # result = es_client.search(index="movies", body=QUERY)
+    # {elt['_source']['title']: elt['_source']['popularity'] for elt in result["hits"]["hits"]}
+    # return render_template('generic.html')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000)
     app.run(host='0.0.0.0')
